a12b0a3f78e0710ac690fef983c42fe44f767b77
halley-dev <hfs.java.dev@gmail.com>
2025-07-28 05:01:30 -0300

    fix delei a Missoe repositore e criei Ninja repository

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.java10x.CadastroDeNinja.Missoes;
-
-import dev.java10x.CadastroDeNinja.Ninjas.NinjaService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
-}
